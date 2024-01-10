d8dcc60172232d548ecd4fc1b5e223c888a5a0c3
Andrey <andreiperevertov@yandex.ru>
2024-01-10 20:24:48 +0300

    Lesson_1.2_Forms_and_formats_of_data_transfer

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,17 +1,52 @@
 package ru.netology;
 
-import java.io.*;
-import java.net.ServerSocket;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    final  Server server = new Server();
-    server.listen(999);
-  }
+    public static void main(String[] args) throws IOException {
+        final Server server = new Server(9999);
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                final var path = request.getPath().split("\\?")[0];
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+                final var length = Files.size(filePath);
+                final var queryParams = request.getQueryParams();
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Other: " + queryParams + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, responseStream);
+                responseStream.flush();
+            }
+        });
+
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+
+                final var filePath = Path.of("public/postmessages");
+                final var mimeType = Files.probeContentType(filePath);
+                final var length = Files.size(filePath);
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, responseStream);
+                responseStream.flush();
+            }
+        });
+        server.listen();
+    }
 }
 
 
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,59 +1,76 @@
 package ru.netology;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server {
-    ExecutorService executor;
-
-    public Server() {
-        this.executor = Executors.newFixedThreadPool(64);
-    }
+public class Server implements Handler {
+    public static final String GET = "GET";
+    public static final String POST = "POST";//используем только неизменяемые методы GET и POST
     final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-    public void listen(int port) {
-        executor.submit(() -> startingSocket(port));
+    //список эндпойнтов их исходной задачи, не трогаем
+    int portNumber;
+    ExecutorService executor;
+    String notFound = "HTTP/1.1 404 Not Found\r\n" +
+            "Content-Length: 0\r\n" +
+            "Connection: close\r\n" +
+            "\r\n";
+    private final Map<String, Map<String, Handler>> handlers = Map.of(GET, new HashMap<>(), POST, new HashMap<>());
+    public Server(int portNumber) {
+        this.executor = Executors.newFixedThreadPool(64);
+        this.portNumber = portNumber;
+
     }
+    public void listen() {
+        executor.execute(() -> startingSocket(portNumber));
+    } // метод для передачи прослушиваемого порта
+
     public void startingSocket(int port) {
         try (final var serverSocket = new ServerSocket(port)) {
             while (true) {
                 try (
                         final var socket = serverSocket.accept();
-                        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        final var out = new BufferedOutputStream(socket.getOutputStream());
+                        final var in = new BufferedInputStream((socket.getInputStream()));
+                        final var out = new BufferedOutputStream(socket.getOutputStream())
                 ) {
-                    // read only request line for simplicity
-                    // must be in form GET /path HTTP/1.1
-                    final var requestLine = in.readLine();
-                    final var parts = requestLine.split(" ");
 
-                    if (parts.length != 3) {
-                        // just close socket
+                    Request request = Request.createRequest(in);
+                    System.out.println(request);
+                    if (request == null) {
+                        out.write((notFound).getBytes());
+                        out.flush();
+                        continue;
+
+                    }
+                    final var methodHTTP = request.getMethod();
+                    final var path = request.getPath().split("\\?")[0];
+                    System.out.println(path);
+                    final var filePath = Path.of(".", "public", path);
+                    final var mimeType = Files.probeContentType(filePath);
+
+                    if (handlers.get(methodHTTP).containsKey(path)) {
+                        handlers.get(methodHTTP).get(path).handle(request, out);
                         continue;
                     }
-
-                    final var path = parts[1];
-                    if (!validPaths.contains(path)) {
-                        out.write((
-                                "HTTP/1.1 404 Not Found\r\n" +
-                                        "Content-Length: 0\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
+                    if (!validPaths.contains(path) && !handlers.get(POST).containsKey(path) && !handlers.get(GET).containsKey(path)) {
+                        out.write((notFound).getBytes());
                         out.flush();
                         continue;
                     }
-
-                    final var filePath = Path.of(".", "public", path);
-                    final var mimeType = Files.probeContentType(filePath);
+                    if (handlers.get(methodHTTP).containsKey(path)) {
+                        handlers.get(methodHTTP).get(path).handle(request, out);
+                        continue;
+                    }
 
                     // special case for classic
                     if (path.equals("/classic.html")) {
@@ -73,7 +90,6 @@ public class Server {
                         out.flush();
                         continue;
                     }
-
                     final var length = Files.size(filePath);
                     out.write((
                             "HTTP/1.1 200 OK\r\n" +
@@ -84,6 +100,8 @@ public class Server {
                     ).getBytes());
                     Files.copy(filePath, out);
                     out.flush();
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
                 }
             }
         } catch (
@@ -91,4 +109,22 @@ public class Server {
             e.printStackTrace();
         }
     }
+
+    public void addHandler(String method, String path, Handler handler) {
+        if (handlers.containsKey(method)) {
+            handlers.get(method).put(path, handler);
+
+        } else {
+            System.out.println("Сервер принимает запросы только по методам GET и POST");
+        }
+    }
+
+    @Override
+    public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+        /*final var path = request.getPath();
+        final var filePath = Path.of(".", "public", path);
+        final var mimeType = Files.probeContentType(filePath);
+        final var length = Files.size(filePath);*/
+    }
 }
+
